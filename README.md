a43621343a1d353538e2d378a262514bfd04f571
Pauliina Anttila <pau.anttila@gmail.com>
2019-07-12 14:04:33 +0300

    Draw the grid

---
+++ README.md
@@ -0,0 +1,11 @@
+# Snake game with Python
+
+## Installation
+
+`apt-get install python-tk`
+
+`pip install -r requirements.txt`
+
+## Run
+
+`python snakegame.py`
---
+++ snakegame.py
@@ -0,0 +1,82 @@
+import pygame
+import math
+import random
+import tkinter as tk
+from tkinter import messagebox
+
+
+class Cube(object):
+    rows = 0
+    w = 0
+
+    def __init__(self, start, dirnx=1, dirny=0, color=(255, 0, 0)):
+        pass
+
+    def move(self, firnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class Snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def addCube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+    x = 0
+    y = 0
+    for l in range(rows):
+        x += sizeBtwn
+        y += sizeBtwn
+
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+
+
+def redrawWindow(surface):
+    global rows, width
+    surface.fill((0, 0, 0))
+    drawGrid(width, rows, surface)
+    pygame.display.update()
+
+
+def randomSnack(rows, items):
+    pass
+
+
+def message_mov(subject, content):
+    pass
+
+
+def main():
+    global width, rows
+    width = 500
+    rows = 20
+    win = pygame.display.set_mode((width, width))
+    s = Snake((255, 0, 0), (10, 10))
+
+    flag = True
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redrawWindow(win)
+
+
+main()
