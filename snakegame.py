e3b0b98d5837716e532db7a406f2e89e3642636e
Pauliina Anttila <pau.anttila@gmail.com>
2019-07-12 15:23:42 +0300

    Add snack

--- snakegame.py
+++ snakegame.py
@@ -123,15 +123,26 @@ def drawGrid(w, rows, surface):
 
 
 def redrawWindow(surface):
-    global rows, width
+    global rows, width, snake, snack
     surface.fill((0, 0, 0))
     snake.draw(surface)
+    snack.draw(surface)
     drawGrid(width, rows, surface)
     pygame.display.update()
 
 
-def randomSnack(rows, items):
-    pass
+def randomSnack(rows, item):
+    positions = item.body
+    while True:
+        # generate random position
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        # prevent snack appearing on the snake
+        if len(list(filter(lambda z: z.pos == (x, y), positions))) > 0:
+            continue
+        else:
+            break
+    return (x, y)
 
 
 def message_mov(subject, content):
@@ -139,11 +150,12 @@ def message_mov(subject, content):
 
 
 def main():
-    global width, rows, snake
+    global width, rows, snake, snack
     width = 500
     rows = 20
     win = pygame.display.set_mode((width, width))
     snake = Snake((255, 0, 0), (10, 10))
+    snack = Cube(randomSnack(rows, snake), color=(0, 255, 0))
 
     flag = True
     clock = pygame.time.Clock()
@@ -152,6 +164,9 @@ def main():
         pygame.time.delay(50)
         clock.tick(10)
         snake.move()
+        if snake.body[0].pos == snack.pos:
+            snake.addCube()
+            snack = Cube(randomSnack(rows, snake), color=(0, 255, 0))
         redrawWindow(win)
 
 
